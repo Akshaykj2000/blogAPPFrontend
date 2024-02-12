cccaabceca24e59ada13a877b7c1bd7b4bbe7750
Akshaykj2000 <akshaykj2000@gmail.com>
2024-02-12 10:48:02 +0530

    front end of blog

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(mainPage());
+}
+class mainPage extends StatelessWidget {
+  const mainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: signinpage(),
+    );
+  }
+}
---
+++ lib/pages/signinPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class signinPage extends StatefulWidget {
+  const signinPage({super.key});
+
+  @override
+  State<signinPage> createState() => _signinPageState();
+}
+
+class _signinPageState extends State<signinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
